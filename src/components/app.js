eae568138ea04d43af09ff524d7ace8d654660c8
Sean Lee <seanboramlee@gmail.com>
2017-01-26 08:28:12 -0800

    add submit button to SearchBar

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import SearchBar from '../containers/SearchBar';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <SearchBar />
     );
   }
 }
