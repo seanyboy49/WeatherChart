bca116bdb80939cf184d62fdd24e309c3b4b254f
Sean Lee <seanboramlee@gmail.com>
2017-01-26 15:30:27 -0800

    add switch to reducer_weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,10 @@
-export default function(state=null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state=[], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([action.paload.data]); // concat creates a new instance
+      return [ action.payload.data, ...state] // es6 magic that takes all the entries of state and moves them into the new array
+  }
   return state;
 }
