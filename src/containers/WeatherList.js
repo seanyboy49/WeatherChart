07abc24784f625504709e8742db40f3c866f8d72
Sean Lee <seanboramlee@gmail.com>
2017-01-27 12:06:39 -0800

    add WeatherList container

---
+++ src/containers/WeatherList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    );
+  }
+}
