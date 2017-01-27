4462399f133c1db3c869530c7db5fdeda12bd8ae
Sean Lee <seanboramlee@gmail.com>
2017-01-27 14:19:43 -0800

    add some styling to center all charts

--- src/components/chart.js
+++ src/components/chart.js
@@ -15,7 +15,7 @@ export default (props) => {
         <SparklinesLine color={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
-      <div>{average(props.data)}</div>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   );
 }
