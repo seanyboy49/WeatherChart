acbb672fd6e81c5648123988275d2e4ebd2e1df8
Sean Lee <seanboramlee@gmail.com>
2017-01-27 12:38:47 -0800

    add key to each <tr> in WeatherList

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name
+    return(
+      <tr key={name}>
+        <td >{name}</td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps({ weather }) { // pulls weather out of state.weather
+  return { weather }; // { weather } === { weather: weather a}
+}
+
+export default connect(mapStateToProps)(WeatherList);
